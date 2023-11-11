27b0709c7c830056deda138a7866d19a547905b6
jinta <jintangz@126.com>
2023-11-11 10:57:42 +0800

    测试

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-# This is a sample Python script.
+'''
+Author: jinta jintangz@126.com
+Date: 2023-11-11 10:29:38
+LastEditors: jinta jintangz@126.com
+LastEditTime: 2023-11-11 10:57:31
+filePath: Do not edit
+Description: main program
+version: v1
+Copyright: Copyright (c) by 2023 by kimzhao. All Rights Reserved
+'''
+
+
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name: str|list, age:int):     
+    '''
+    description: 
+    param {str} name
+    param {int} age
+    return {*}
+    '''
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
@@ -14,3 +32,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
